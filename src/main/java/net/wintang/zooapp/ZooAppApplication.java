4ecaab21fbd3af076bce1bcbff37671abeb5c858
WintangZ <vinhttse172042@fpt.edu.vn>
2023-09-17 22:32:59 +0700

    First Commit

---
+++ src/main/java/net/wintang/zooapp/ZooAppApplication.java
@@ -0,0 +1,13 @@
+package net.wintang.zooapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/wintang/zooapp/ZooAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wintang.zooapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
