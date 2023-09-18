665380b535d839c75680e78cb2bef4b86c7974e0
WintangZ <vinhttse172042@fpt.edu.vn>
2023-09-18 09:55:09 +0700

    Added CORS Config

--- src/main/java/net/wintang/zooapp/controller/AccountController.java
+++ src/main/java/net/wintang/zooapp/controller/AccountController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/account")
 public class AccountController {
     private final IAccountService accountService;
