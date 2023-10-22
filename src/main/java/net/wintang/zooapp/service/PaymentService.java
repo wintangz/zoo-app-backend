2a676648d337384d2f039ac5d2e64d7baecfc987
Wintang1111 <vinhttse172042@fpt.edu.vn>
2023-10-23 05:02:23 +0700

    Modify PaymentService.

--- src/main/java/net/wintang/zooapp/service/PaymentService.java
+++ src/main/java/net/wintang/zooapp/service/PaymentService.java
@@ -59,7 +59,7 @@ public class PaymentService implements IPaymentService {
         Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
 
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-        formatter.setTimeZone(TimeZone.getTimeZone("Etc/GMT+7"));
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT+7"));
         String vnp_CreateDate = formatter.format(cld.getTime());
 
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
